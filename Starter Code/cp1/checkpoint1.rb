95c8615979690d882321f29052c89aa766a0fc83
Oops <16302010059@fudan.edu.cn>
2018-11-25 12:31:54 +0800

    根据readme注释代码

--- Starter Code/cp1/checkpoint1.rb
+++ Starter Code/cp1/checkpoint1.rb
@@ -1,4 +1,4 @@
-#!/usr/localbin/ruby
+#!/usr/local/bin/ruby
 
 
 # tests ability of your peer to send a correct WHOHAS request
@@ -118,9 +118,9 @@ end
 
 
 # comment out these 3 lines, once you've read the README
-puts "Make sure you carefully read the README!"
-puts "Then comment these lines out.  Exiting..."
-exit 0
+# puts "Make sure you carefully read the README!"
+# puts "Then comment these lines out.  Exiting..."
+# exit 0
 
 if (!File.exists?("peer"))
 	puts "Error: need to have binary named 'peer' in this directory"
